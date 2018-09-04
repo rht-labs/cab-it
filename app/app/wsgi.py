61d80278f1e57ecbc13221e246ab44d7d0735ca6
Patrick Carney <patrick.s.carney@gmail.com>
2018-09-04 19:33:08 -0400

    app

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -1,23 +1,22 @@
 from flask import Flask, templating, render_template, request
 import api
 
-def application():
+app = Flask(__name__, template_folder='templates/')
 
-    app = Flask(__name__, template_folder='templates/')
+################################################################################
+# Storing something like a counter is really a job for redis or something
+# like that - but I didn't want to introduce extra dependencies in this example.
+# Don't use application context for real storage!
+################################################################################
 
-    ################################################################################
-    # Storing something like a counter is really a job for redis or something 
-    # like that - but I didn't want to introduce extra dependencies in this example.
-    # Don't use application context for real storage!
-    ################################################################################
+app.config.counter = 1
 
-    app.config.counter = 1
+app.register_blueprint(api, url_prefix='/api')
 
-    app.register_blueprint(api, url_prefix='/api')
+# Templating and template inheritance
+@app.route('/')
+def index():
+    return render_template('index.html', counter=app.config.counter)
 
-    # Templating and template inheritance
-    @app.route('/')
-    def index():
-        return render_template('index.html', counter=app.config.counter)
-
-    return app
+if __name__ == "__main__":
+    app.run()
