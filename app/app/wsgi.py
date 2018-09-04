94afbc6a2b7dd1c9bf075b5b56e67edf5402a068
Patrick Carney <patrick.s.carney@gmail.com>
2018-09-04 19:40:31 -0400

    hello world

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -1,22 +1,32 @@
-from flask import Flask, templating, render_template, request
-import api
+# from flask import Flask, templating, render_template, request
+# import api
+#
+# def application():
+#
+#     app = Flask(__name__, template_folder='templates/')
+#
+#     ################################################################################
+#     # Storing something like a counter is really a job for redis or something
+#     # like that - but I didn't want to introduce extra dependencies in this example.
+#     # Don't use application context for real storage!
+#     ################################################################################
+#
+#     app.config.counter = 1
+#
+#     app.register_blueprint(api, url_prefix='/api')
+#
+#     # Templating and template inheritance
+#     @app.route('/')
+#     def index():
+#         return render_template('index.html', counter=app.config.counter)
+#
+#     return app
+from flask import Flask
+application = Flask(__name__)
 
-app = Flask(__name__, template_folder='templates/')
-
-################################################################################
-# Storing something like a counter is really a job for redis or something
-# like that - but I didn't want to introduce extra dependencies in this example.
-# Don't use application context for real storage!
-################################################################################
-
-app.config.counter = 1
-
-app.register_blueprint(api, url_prefix='/api')
-
-# Templating and template inheritance
-@app.route('/')
-def index():
-    return render_template('index.html', counter=app.config.counter)
+@application.route("/")
+def hello():
+    return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
