800339a71efa122ff4758ef6441a7060f1ae830c
Patrick Carney <patrick.s.carney@gmail.com>
2018-09-06 20:20:29 -0400

    formatting for python

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -1,31 +1,31 @@
 import pytest
 
-def test_initial_state(client, app):
 
+def test_initial_state(client, app):
     result = client.get('/api/counter')
     assert b'1' in result.data
 
-def test_set_counter(client, app):
 
+def test_set_counter(client, app):
     result = client.get('/api/set_counter/10')
     assert b'10' == result.data
 
-def test_increment_get(client, app):
 
+def test_increment_get(client, app):
     client.get('/api/set_counter/1')
     client.get('/api/increment_counter/5')
     currentState = client.get('/api/counter')
     assert b'6' == currentState.data
 
-def test_decrement_get(client, app):
 
+def test_decrement_get(client, app):
     client.get('/api/set_counter/5')
     client.get('/api/decrement_counter/1')
     currentState = client.get('/api/counter')
     assert b'4' == currentState.data
 
-def test_increment_post(client, app):
 
+def test_increment_post(client, app):
     client.get('/api/set_counter/1')
     client.post('/api/increment_counter', data=dict(
         amount=5
@@ -33,8 +33,8 @@ def test_increment_post(client, app):
     currentState = client.get('/api/counter')
     assert b'6' == currentState.data
 
-def test_decrement_post(client, app):
 
+def test_decrement_post(client, app):
     client.get('/api/set_counter/5')
     client.post('/api/decrement_counter', data=dict(
         amount=1
